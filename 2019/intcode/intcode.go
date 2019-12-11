a9b4958998b88301769132243898785f8f8f7126
James Heller <jaheller5@gmail.com>
2019-12-10 23:56:47 -0800

    2019 Day 9 part 1

--- 2019/intcode/intcode.go
+++ 2019/intcode/intcode.go
@@ -2,7 +2,6 @@ package intcode
 
 import (
 	"fmt"
-	"math"
 	"strconv"
 )
 
@@ -19,6 +18,8 @@ const (
 	lt  Opcode = 7
 	eq  Opcode = 8
 
+	arb Opcode = 9
+
 	halt Opcode = 99
 )
 
@@ -27,19 +28,20 @@ type Mode int
 const (
 	pos Mode = 0
 	imd Mode = 1
+	rel Mode = 2
 )
 
 type instruction struct {
 	opcode Opcode
 
-	val1Adr  int
+	val1     int
 	val1Mode Mode
 
-	val2Adr  int
+	val2     int
 	val2Mode Mode
 
-	destAdr  int
-	destMode Mode
+	val3     int
+	val3Mode Mode
 }
 
 func (i instruction) instructionPointerOffset() int {
@@ -52,7 +54,7 @@ func (i instruction) instructionPointerOffset() int {
 	return -1
 }
 
-func getInstruction(instructionPointer *int, program []int) (instruction, error) {
+func getInstruction(instructionPointer *int, program map[int]int) (instruction, error) {
 	ip := *instructionPointer
 
 	// Parse Opcode and Mode
@@ -90,6 +92,7 @@ func getInstruction(instructionPointer *int, program []int) (instruction, error)
 		opcode != jmf &&
 		opcode != lt &&
 		opcode != eq &&
+		opcode != arb &&
 		opcode != halt {
 		return instruction{}, fmt.Errorf("unknown opcode: %d", opcode)
 	}
@@ -102,21 +105,21 @@ func getInstruction(instructionPointer *int, program []int) (instruction, error)
 		opcode: opcode,
 	}
 	if opcode == add || opcode == mul || opcode == lt || opcode == eq {
-		inst.val1Adr = program[ip+1]
+		inst.val1 = program[ip+1]
 		inst.val1Mode = defaultModes[0]
-		inst.val2Adr = program[ip+2]
+		inst.val2 = program[ip+2]
 		inst.val2Mode = defaultModes[1]
-		inst.destAdr = program[ip+3]
-		inst.destMode = defaultModes[2]
+		inst.val3 = program[ip+3]
+		inst.val3Mode = defaultModes[2]
 		*instructionPointer = ip + 4
 	} else if opcode == jmt || opcode == jmf {
-		inst.val1Adr = program[ip+1]
+		inst.val1 = program[ip+1]
 		inst.val1Mode = defaultModes[0]
-		inst.destAdr = program[ip+2]
-		inst.destMode = defaultModes[1]
+		inst.val2 = program[ip+2]
+		inst.val2Mode = defaultModes[1]
 		*instructionPointer = ip + 3
-	} else if opcode == sav || opcode == out {
-		inst.val1Adr = program[ip+1]
+	} else if opcode == sav || opcode == out || opcode == arb {
+		inst.val1 = program[ip+1]
 		inst.val1Mode = defaultModes[0]
 		*instructionPointer = ip + 2
 	}
@@ -124,204 +127,144 @@ func getInstruction(instructionPointer *int, program []int) (instruction, error)
 	return inst, nil
 }
 
-func addValues(inst instruction, program []int) {
-	var val1 int
-	switch inst.val1Mode {
-	case pos:
-		val1 = program[inst.val1Adr]
-	case imd:
-		val1 = inst.val1Adr
-	}
-
-	var val2 int
-	switch inst.val2Mode {
-	case pos:
-		val2 = program[inst.val2Adr]
-	case imd:
-		val2 = inst.val2Adr
-	}
+func addValues(inst instruction, memory map[int]int, relativeBase int) {
+	val1 := getValue(inst.val1, inst.val1Mode, memory, relativeBase)
+	val2 := getValue(inst.val2, inst.val2Mode, memory, relativeBase)
 
 	sum := val1 + val2
-	// Can a destination be immediate mode?
-	if inst.destMode == imd {
-		fmt.Printf("immediate add dest: %+v\n", inst)
-	}
-	program[inst.destAdr] = sum
+
+	setValue(sum, inst.val3, inst.val3Mode, memory, relativeBase)
 }
 
-func mulValues(inst instruction, program []int) {
-	var val1 int
-	switch inst.val1Mode {
-	case pos:
-		val1 = program[inst.val1Adr]
-	case imd:
-		val1 = inst.val1Adr
-	}
-
-	var val2 int
-	switch inst.val2Mode {
-	case pos:
-		val2 = program[inst.val2Adr]
-	case imd:
-		val2 = inst.val2Adr
-	}
+func mulValues(inst instruction, memory map[int]int, relativeBase int) {
+	val1 := getValue(inst.val1, inst.val1Mode, memory, relativeBase)
+	val2 := getValue(inst.val2, inst.val2Mode, memory, relativeBase)
 
 	product := val1 * val2
-	// Can a destination be immediate mode?
-	if inst.destMode == imd {
-		fmt.Printf("immediate mul dest: %+v\n", inst)
-	}
-	program[inst.destAdr] = product
+
+	setValue(product, inst.val3, inst.val3Mode, memory, relativeBase)
 }
 
-// Can an input be immediate mode?
-func saveValues(inst instruction, in int, program []int) {
-	if inst.val1Mode == imd {
-		fmt.Printf("immediate save: %+v\n", inst)
-	}
-	program[inst.val1Adr] = in
+func saveValues(inst instruction, in int, memory map[int]int, relativeBase int) {
+	setValue(in, inst.val1, inst.val1Mode, memory, relativeBase)
 }
 
 // Immediate output is verified as possible
-func outputValues(inst instruction, program []int) int {
-	switch inst.val1Mode {
-	case pos:
-		return program[inst.val1Adr]
-	case imd:
-		return inst.val1Adr
-	}
-
-	return -1 * math.MaxInt64
+func outputValues(inst instruction, memory map[int]int, relativeBase int) int {
+	return getValue(inst.val1, inst.val1Mode, memory, relativeBase)
 }
 
-func jumpIfTrue(instructionPointer *int, inst instruction, program []int) {
-	var val1 int
-	switch inst.val1Mode {
-	case pos:
-		val1 = program[inst.val1Adr]
-	case imd:
-		val1 = inst.val1Adr
-	}
-
-	var dest int
-	switch inst.destMode {
-	case pos:
-		dest = program[inst.destAdr]
-	case imd:
-		dest = inst.destAdr
-	}
+func jumpIfTrue(instructionPointer *int, inst instruction, memory map[int]int, relativeBase int) {
+	val1 := getValue(inst.val1, inst.val1Mode, memory, relativeBase)
+	val2 := getValue(inst.val2, inst.val2Mode, memory, relativeBase)
 
 	if val1 != 0 {
-		*instructionPointer = dest
+		*instructionPointer = val2
 	}
 }
 
-func jumpIfFalse(instructionPointer *int, inst instruction, program []int) {
-	var val1 int
-	switch inst.val1Mode {
-	case pos:
-		val1 = program[inst.val1Adr]
-	case imd:
-		val1 = inst.val1Adr
-	}
-
-	var dest int
-	switch inst.destMode {
-	case pos:
-		dest = program[inst.destAdr]
-	case imd:
-		dest = inst.destAdr
-	}
+func jumpIfFalse(instructionPointer *int, inst instruction, memory map[int]int, relativeBase int) {
+	val1 := getValue(inst.val1, inst.val1Mode, memory, relativeBase)
+	val2 := getValue(inst.val2, inst.val2Mode, memory, relativeBase)
 
 	if val1 == 0 {
-		*instructionPointer = dest
+		*instructionPointer = val2
 	}
 }
 
-func lessThan(inst instruction, program []int) {
-	var val1 int
-	switch inst.val1Mode {
-	case pos:
-		val1 = program[inst.val1Adr]
-	case imd:
-		val1 = inst.val1Adr
-	}
-
-	var val2 int
-	switch inst.val2Mode {
-	case pos:
-		val2 = program[inst.val2Adr]
-	case imd:
-		val2 = inst.val2Adr
-	}
+func lessThan(inst instruction, memory map[int]int, relativeBase int) {
+	val1 := getValue(inst.val1, inst.val1Mode, memory, relativeBase)
+	val2 := getValue(inst.val2, inst.val2Mode, memory, relativeBase)
 
 	destVal := 0
 	if val1 < val2 {
 		destVal = 1
 	}
 
-	program[inst.destAdr] = destVal
+	setValue(destVal, inst.val3, inst.val3Mode, memory, relativeBase)
 }
 
-func equals(inst instruction, program []int) {
-	var val1 int
-	switch inst.val1Mode {
-	case pos:
-		val1 = program[inst.val1Adr]
-	case imd:
-		val1 = inst.val1Adr
-	}
-
-	var val2 int
-	switch inst.val2Mode {
-	case pos:
-		val2 = program[inst.val2Adr]
-	case imd:
-		val2 = inst.val2Adr
-	}
+func equals(inst instruction, memory map[int]int, relativeBase int) {
+	val1 := getValue(inst.val1, inst.val1Mode, memory, relativeBase)
+	val2 := getValue(inst.val2, inst.val2Mode, memory, relativeBase)
 
 	destVal := 0
 	if val1 == val2 {
 		destVal = 1
 	}
 
-	program[inst.destAdr] = destVal
+	setValue(destVal, inst.val3, inst.val3Mode, memory, relativeBase)
 }
 
-func RunProgram(tag int, memory []int, input <-chan int, output chan<- int) error {
+func adjustRelativeBase(inst instruction, memory map[int]int, relativeBase *int) {
+	switch inst.val1Mode {
+	case pos:
+		*relativeBase += memory[inst.val1]
+	case imd:
+		*relativeBase += inst.val1
+	case rel:
+		*relativeBase += memory[*relativeBase+inst.val1]
+	}
+}
+
+func getValue(val int, mode Mode, memory map[int]int, relativeBase int) int {
+	value := 0
+	switch mode {
+	case pos:
+		value = memory[val]
+	case imd:
+		value = val
+	case rel:
+		value = memory[val+relativeBase]
+	}
+
+	return value
+}
+
+func setValue(input, val int, mode Mode, memory map[int]int, relativeBase int) {
+	switch mode {
+	case pos:
+		memory[val] = input
+	case imd:
+		fmt.Println("immediate mode used for setValue")
+	case rel:
+		memory[val+relativeBase] = input
+	}
+}
+
+func RunProgram(tag int, memory map[int]int, input <-chan int, output chan<- int) error {
 	instructionPointer := 0
 	inst, err := getInstruction(&instructionPointer, memory)
 	if err != nil {
 		return err
 	}
 
-	for inst.opcode != halt {
+	relativeBase := 0
 
+	for inst.opcode != halt {
 		switch inst.opcode {
 		case add:
-			addValues(inst, memory)
+			addValues(inst, memory, relativeBase)
 		case mul:
-			mulValues(inst, memory)
+			mulValues(inst, memory, relativeBase)
 		case sav:
-			// fmt.Printf("%d: waiting for input\n", tag)
 			in := <-input
-			saveValues(inst, in, memory)
-			// fmt.Printf("%d: input received: %d\n", tag, in)
+			saveValues(inst, in, memory, relativeBase)
 		case out:
-			out := outputValues(inst, memory)
-			// fmt.Printf("%d: output: %d\n", tag, out)
+			out := outputValues(inst, memory, relativeBase)
 			output <- out
 		case jmt:
-			jumpIfTrue(&instructionPointer, inst, memory)
+			jumpIfTrue(&instructionPointer, inst, memory, relativeBase)
 		case jmf:
-			jumpIfFalse(&instructionPointer, inst, memory)
+			jumpIfFalse(&instructionPointer, inst, memory, relativeBase)
 		case lt:
-			lessThan(inst, memory)
+			lessThan(inst, memory, relativeBase)
 		case eq:
-			equals(inst, memory)
+			equals(inst, memory, relativeBase)
+		case arb:
+			adjustRelativeBase(inst, memory, &relativeBase)
 		}
 
-		// instructionPointer += inst.instructionPointerOffset()
 		inst, err = getInstruction(&instructionPointer, memory)
 		if err != nil {
 			return err
